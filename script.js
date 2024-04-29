cb226ab158f4479102e6406a60653f1c3528e742
IamProbablyAsian <65319357+IamProbablyAsian@users.noreply.github.com>
2024-04-29 10:31:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function startTimer(duration) {
             minutes = parseInt(timer / 60, 10);
             seconds = parseInt(timer % 60, 10);
 
-            minutes = minutes < 10 ? "0" + minutes : minutes;
+            minutes = minutes < 7 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
 
             document.getElementById('timer').textContent = minutes + ":" + seconds;
